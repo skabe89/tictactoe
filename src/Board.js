bbc79da90217ca0bc3f2ff6e1242703d42e26ba1
Sean Abe <github email address>
2021-07-08 18:34:27 -0400

    board array

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function src (){
+export default function Board (){
+
+    const [player, setPlayer] = useState("X")
+    const [board, setBoard] = useState(['', '', '', '', '', '', '', '', ''])
+
+    const changePlayer = () => {
+        player === 'X' ? setPlayer('O') : setPlayer('X')
+    }
 
     return(
         <div>
             <h1>Hello World</h1>
+            <h1> {board.join(' || ')} </h1>
+            <h1>{player}</h1>
         </div>
 
     )
