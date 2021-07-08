31907a2952426d8095d26ab6cc1b9acfeeac873e
Sean Abe <github email address>
2021-07-08 18:38:24 -0400

    board displayed in-line

--- src/Board.js
+++ src/Board.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 export default function Board (){
 
     const [player, setPlayer] = useState("X")
-    const [board, setBoard] = useState(['', '', '', '', '', '', '', '', ''])
+    const [board, setBoard] = useState(['.', '.', '.', '.', '.', '.', '.', '.', '.'])
 
     const changePlayer = () => {
         player === 'X' ? setPlayer('O') : setPlayer('X')
@@ -12,8 +12,18 @@ export default function Board (){
     return(
         <div>
             <h1>Hello World</h1>
-            <h1> {board.join(' || ')} </h1>
+            {/* <h1> {board.join(' || ')} </h1> */}
             <h1 onClick={changePlayer} > {player} </h1>
+
+            <h1>{board[0]}</h1>
+            <h1>{board[1]}</h1>
+            <h1>{board[2]}</h1>
+            <h1>{board[3]}</h1>
+            <h1>{board[4]}</h1>
+            <h1>{board[5]}</h1>
+            <h1>{board[6]}</h1>
+            <h1>{board[7]}</h1>
+            <h1>{board[8]}</h1>
 
         </div>
 
