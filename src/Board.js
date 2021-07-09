9dbe593e0c1f2f561e4d40ca2d64f67429323064
Sean Abe <github email address>
2021-07-09 10:10:58 -0400

    board css prelim

--- src/Board.js
+++ src/Board.js
@@ -23,19 +23,21 @@ export default function Board (){
 
     return(
         <div>
-            <h1>Hello World</h1>
+            <h1>Tic Tac Toe</h1>
             {/* <h1> {board.join(' || ')} </h1> */}
-            <h1 onClick={changePlayer} > {player} </h1>
+            <h1 onClick={changePlayer} > {player}'s Turn </h1>
 
-            <h1 onClick={() => placeCharacter(0)}>{board[0]}</h1>
-            <h1 onClick={() => placeCharacter(1)}>{board[1]}</h1>
-            <h1 onClick={() => placeCharacter(2)}>{board[2]}</h1>
-            <h1 onClick={() => placeCharacter(3)}>{board[3]}</h1>
-            <h1 onClick={() => placeCharacter(4)}>{board[4]}</h1>
-            <h1 onClick={() => placeCharacter(5)}>{board[5]}</h1>
-            <h1 onClick={() => placeCharacter(6)}>{board[6]}</h1>
-            <h1 onClick={() => placeCharacter(7)}>{board[7]}</h1>
-            <h1 onClick={() => placeCharacter(8)}>{board[8]}</h1>
+            <div className="Board-square">
+                <h1 className="Board-cell" onClick={() => placeCharacter(0)}>{board[0]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(1)}>{board[1]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(2)}>{board[2]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(3)}>{board[3]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(4)}>{board[4]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(5)}>{board[5]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(6)}>{board[6]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(7)}>{board[7]}</h1>
+                <h1 className="Board-cell" onClick={() => placeCharacter(8)}>{board[8]}</h1>
+            </div>
 
         </div>
 
