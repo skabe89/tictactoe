13b789c4ad07ee2df0d56c315819c9bf55b8fcfa
Sean Abe <skabe89@gmail.com>
2021-07-14 22:53:04 -0400

    git email

--- src/Board.js
+++ src/Board.js
@@ -4,6 +4,7 @@ export default function Board (){
 
     const [player, setPlayer] = useState("X")
     const [board, setBoard] = useState(['.', '.', '.', '.', '.', '.', '.', '.', '.'])
+
     const wins = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 4, 8], [2, 4, 6],
@@ -19,6 +20,7 @@ export default function Board (){
             let newBoard = [...board]
             newBoard[index] = player
             setBoard(newBoard)
+            checkWin()
             changePlayer()
         }
         else {
@@ -27,8 +29,14 @@ export default function Board (){
     }
 
     const checkWin = () => {
-        
-        
+        let won = false
+        wins.forEach(combo => {
+            if(board[combo[0]] === player && board[combo[1]] === player && board[combo[2]] === player){
+                console.log(board[combo[0]])
+                won = true
+            }
+        })
+        return won
     }
 
     return(
